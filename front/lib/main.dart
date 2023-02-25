17ea8b814a7abaf9edcf756e0ddbed560e6792f5
Savelev <carry2002life@mail.ru>
2023-02-26 02:06:45 +0300

    set min size of desktop version, also changes request body

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -9,14 +9,14 @@ import 'package:velocity_x/velocity_x.dart';
 import 'utils/routes.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // if (defaultTargetPlatform == TargetPlatform.windows || defaultTargetPlatform == TargetPlatform.linux || defaultTargetPlatform == TargetPlatform.macOS) {
-  //     setWindowTitle('Ide Killer');
-  //     setWindowMinSize(const Size(1280,720));
-  //     Future<Null>.delayed(const Duration(seconds: 1), () {
-  //       setWindowFrame(Rect.fromCenter(center: const Offset(1000, 500), width: 1280, height: 720));
-  //     });
-  // }
+  WidgetsFlutterBinding.ensureInitialized();
+  if (defaultTargetPlatform == TargetPlatform.windows || defaultTargetPlatform == TargetPlatform.linux || defaultTargetPlatform == TargetPlatform.macOS) {
+      setWindowTitle('Ide Killer');
+      setWindowMinSize(const Size(1280,720));
+      Future<Null>.delayed(const Duration(seconds: 1), () {
+        setWindowFrame(Rect.fromCenter(center: const Offset(1000, 500), width: 1280, height: 720));
+      });
+  }
 
   setPathUrlStrategy();
   runApp(MaterialApp.router(
