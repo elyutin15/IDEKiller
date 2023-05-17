295f9859de470ea648dc0d4c71968bb312f24236
elyutinmaks <elyutin15@gmail.com>
2023-05-17 18:19:19 +0300

    С++, С теперь тоже работают, потестил только плюсы

--- back/compiler/src/main/java/mirea/model/Code.java
+++ back/compiler/src/main/java/mirea/model/Code.java
@@ -1,19 +1,20 @@
 package mirea.model;
 
 
-import com.fasterxml.jackson.annotation.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Embeddable;
 import lombok.Data;
-import lombok.SneakyThrows;
+import lombok.NoArgsConstructor;
 
 @Data
+@Embeddable
+@NoArgsConstructor
 public class Code {
     String code;
 
     Language language;
 
-    @JsonIgnore
-    ObjectMapper mapper;
 
     @JsonCreator
     public Code(
@@ -22,13 +23,5 @@ public class Code {
     ) {
         this.code = code;
         this.language = Language.parse(language);
-        mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-    }
-
-    @SneakyThrows
-    @Override
-    public String toString() {
-        return mapper.writeValueAsString(this);
     }
 }
--- back/compiler/src/main/java/mirea/model/CodeDto.java
+++ back/compiler/src/main/java/mirea/model/CodeDto.java
@@ -1,16 +1,20 @@
 package mirea.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
+@Entity
 @NoArgsConstructor
 public class CodeDto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
     Long id;
 
     @Embedded
@@ -18,4 +22,13 @@ public class CodeDto {
 
     Long userid;
 
+
+    @JsonCreator
+    public CodeDto(
+            @JsonProperty("code") Code code,
+            @JsonProperty("id") Long id
+    ) {
+        this.code = code;
+        this.userid = id;
+    }
 }
