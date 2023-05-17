7c19641d1397b9d46b39a36240675cc43584c088
Maks <mzlotnikov2004@gmail.com>
2023-05-17 11:22:21 +0300

    Временное решение красного экрана

--- front/lib/utils/userPreferences.dart
+++ front/lib/utils/userPreferences.dart
@@ -4,3 +4,5 @@ import 'package:idekiller/controllers/model.dart';
 
 User user = User(profilePic: '', name: 'Masss', number: '123', students: [], teachers: [], id: 0, about: 'qwe rty');
 bool isAuthorized = false;
+
+
