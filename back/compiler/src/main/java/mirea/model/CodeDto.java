dae5d93a7afa41894c500a3ad7bc2e3612223777
elyutinmaks <elyutin15@gmail.com>
2023-05-24 11:57:50 +0300

    Удаление userid из модели CodeDto

--- back/compiler/src/main/java/mirea/model/CodeDto.java
+++ back/compiler/src/main/java/mirea/model/CodeDto.java
@@ -1,6 +1,7 @@
 package mirea.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class CodeDto {
     @Embedded
     Code code;
 
+    @JsonIgnore
     Long userid;
 
     String name;
